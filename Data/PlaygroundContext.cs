892052acef09b710354c99102437de2f60d09375
szybkiwx <andrzej.dajnowski@gmail.com>
2014-05-05 22:01:25 +0200

    fixing

---
+++ Data/PlaygroundContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground.Data
+{
+    public class PlaygroundContext : DbContext
+    {
+        public DbSet<Tag> TagSet { get; set; }
+
+        public DbSet<Expense> ExpenseSet { get; set; }
+
+        public PlaygroundContext()
+            : base("PlaygroundDb")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tag>()
+            .HasMany(tag => tag.Expenses)
+            .WithMany(exp => exp.Tags)
+            .Map(m => m.MapLeftKey("Id")
+            .MapRightKey("Id")
+            .ToTable("TagToExpense"));
+        }
+    }
+}
